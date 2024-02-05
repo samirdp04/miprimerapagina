7f6c13161ff35882065e215c959053a3dab9ba24
Samir Del Portillo <samirdelportillo01@gmail.com>
2024-02-05 11:35:53 -0500

    Initial commit

---
+++ sexo.js
@@ -0,0 +1,42 @@
+let menu_responsive = document.querySelector(".checkbtn");
+
+menu_responsive.onclick = function(){
+    navBar = document.querySelector(".navbar");
+    navBar.classList.toggle("active");
+}
+
+const nombre = document.getElementById("name");
+const email = document.getElementById("email");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warnings");
+
+//function validarFormulario( ) {
+    //let warnings = ""
+    //let entrar = false
+    //let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    //if (nombre.value.length < 3) {
+        //warnings += `El nombre debe contener más de 3 caracteres`;
+        //valido = false;
+    //}
+//}
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
+    if(nombre.value.length < 2){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
+    if(!regexEmail.test(email.value)){
+        warnings += `El Email no es valido <br>`
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }
+})
